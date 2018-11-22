04effc99cb662b9909bc2c9fb74b93bebba28bb2
hanzecheng <xx@xx.com>
2018-11-22 19:58:00 +0800

    http2

---
+++ http2/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/request", func(c echo.Context) error {
+		req := c.Request()
+		format := `
+		  <code>
+			Protocol: %s<br>
+			Host: %s<br>
+			Remote Address: %s<br>
+			Method: %s<br>
+			Path: %s<br>
+		  </code>
+		`
+		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
+	})
+
+	err := e.StartTLS(":1234", "cert.pem", "key.pem")
+	if err != nil {
+		panic(err)
+	}
+}
