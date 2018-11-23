4665547d4b0bf0495ee2ca2f0feb3f72215ecdab
hanzecheng <xx@xx.com>
2018-11-23 21:38:02 +0800

    connection

--- httpconnection/server/main.go
+++ httpconnection/server/main.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-
-	e.GET("/request", func(c echo.Context) error {
-		req := c.Request()
-		format := `
-		  <code>
-			Protocol: %s<br>
-			Host: %s<br>
-			Remote Address: %s<br>
-			Method: %s<br>
-			Path: %s<br>
-		  </code>
-		`
-		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Cache-Control", "max-age=100")
+			return next(c)
+		}
 	})
-
+	e.Static("/hello", "httpcachecontrol/view")
+	e.File("/hello", "httpcachecontrol/view/*.html")
 	err := e.Start(":2234")
 	if err != nil {
 		panic(err)
