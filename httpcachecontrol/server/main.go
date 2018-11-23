5fc66f0185880cd37e64ad4720c37bc19a25d419
hanzecheng <xx@xx.com>
2018-11-23 15:58:38 +0800

    cache-control

---
+++ httpcachecontrol/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Println("经过了mid")
+			return next(c)
+		}
+	})
+	e.Static("/hello", "httpcachecontrol/view")
+	e.File("/hello", "httpcachecontrol/view/*.html")
+	err := e.Start(":2234")
+	if err != nil {
+		panic(err)
+	}
+}
