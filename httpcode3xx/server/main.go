dc0ddfa932efc728129492052b7d3e711ddb93a7
hanzecheng <xx@xx.com>
2018-11-23 13:42:47 +0800

    302

--- httpcode3xx/server/main.go
+++ httpcode3xx/server/main.go
@@ -8,7 +8,11 @@ func main() {
 	e := echo.New()
 
 	e.GET("/request", func(c echo.Context) error {
-		return c.Redirect(302, "https://www.baidu.com")
+		return c.Redirect(302, "test")
+	})
+
+	e.GET("/test", func(c echo.Context) error {
+		return c.Redirect(302, "test")
 	})
 
 	err := e.Start(":2234")
