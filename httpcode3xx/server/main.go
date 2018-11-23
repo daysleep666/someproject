3941c1792aa9db0837a9480bd04d115622fba7e7
hanzecheng <xx@xx.com>
2018-11-23 13:36:45 +0800

    302

---
+++ httpcode3xx/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/request", func(c echo.Context) error {
+		return c.Redirect(302, "www.baidu.com")
+	})
+
+	err := e.Start(":1234")
+	if err != nil {
+		panic(err)
+	}
+}
