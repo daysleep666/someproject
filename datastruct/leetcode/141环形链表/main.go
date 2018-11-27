b0f645ebd3075c7981ce56c96fb501e220f9b526
mac <mac@a.local>
2018-11-27 22:21:33 +0800

    环形指针

--- datastruct/leetcode/141环形链表/main.go
+++ datastruct/leetcode/141环形链表/main.go
@@ -5,6 +5,33 @@ package main
 // 进阶：
 // 你能否不使用额外空间解决此题？
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(_head *ListNode) bool {
+	if _head == nil {
+		return false
+	}
+	for _head.Next != nil {
+		if _head == _head.Next {
+			return true
+		}
+		_head.Next = _head.Next.Next
+	}
+	return false
+}
+
 func main() {
 
 }
