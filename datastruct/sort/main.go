2527a1266b6a4e26e50bd87c355f20384993ef56
mac <mac@a.local>
2019-02-11 21:24:02 +0800

    quicksort

--- datastruct/sort/main.go
+++ datastruct/sort/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/daysleep666/someproject/datastruct/sort/insertionsort"
+	"github.com/daysleep666/someproject/datastruct/sort/quicksort"
 )
 
 var arr []int64 = []int64{3, 2, 4, 1, 5, 8, 6, 0}
 
 func main() {
-	arr = insertionsort.InsertionSort(arr)
+	quicksort.QuickSort(arr)
 	fmt.Println(arr)
 }
--- datastruct/sort/quicksort/quicksort.go
+++ datastruct/sort/quicksort/quicksort.go
@@ -5,22 +5,23 @@ func QuickSort(_arr []int64) {
 }
 
 func quickSort(_arr []int64, _from, _to int64) {
-	if _from <= _to {
+	if _from >= _to {
 		return
 	}
-	partition(_arr, _from, _to)
-	mid := _from + _to
-	quickSort(_arr, _from, mid/2)
-	quickSort(_arr, mid/2+1, _to)
+	mid := partition(_arr, _from, _to)
+	quickSort(_arr, _from, mid-1)
+	quickSort(_arr, mid+1, _to)
 }
 
-func partition(_arr []int64, _from, _to int64) {
+func partition(_arr []int64, _from, _to int64) int64 {
 	pivot := _arr[_to]
-	for i, j := int64(0), int64(0); i < _to-1 && j < _to-1; i++ {
-		if _arr[j] <= pivot {
-			j++
-		} else {
+	i, j := _from, _from
+	for ; j < _to; j++ {
+		if _arr[j] < pivot {
 			_arr[i], _arr[j] = _arr[j], _arr[i]
+			i++
 		}
 	}
+	_arr[_to], _arr[i] = _arr[i], _arr[_to]
+	return i
 }
