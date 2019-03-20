b42652abea12d41904572c20119d715451acea3e
hanzecheng <576129407@qq.com>
2019-03-20 16:28:44 +0800

    .

--- leetcode/60. 第k个排列/main.go
+++ leetcode/60. 第k个排列/main.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 func getPermutation(n int, k int) string {
-	// length := compute(n)
 	nums := make([]byte, n, n)
 	for i := 0; i < n; i++ {
 		nums[i] = byte(i+1) + '0'
 	}
-	strs := d(nums)
-	fmt.Println(strs)
-	return ""
+
+	result := ""
+	for i := n - 1; i >= 0; i-- {
+		index := k / compute(i)
+		fmt.Println(k, compute(i), index)
+		result += string(nums[index])
+		nums = append(nums[:index], nums[index+1:]...)
+		k = k - compute(i)
+	}
+
+	fmt.Println(string(nums))
+	return string(nums)
+}
+
+func compute(n int) int {
+	mul := 1
+	for n > 0 {
+		mul *= n
+		n--
+	}
+	return mul
 }
 
 func d(nums []byte) []string {
@@ -30,15 +47,12 @@ func d(nums []byte) []string {
 	return result
 }
 
-func compute(n int) int {
-	mul := 1
-	for n > 0 {
-		mul *= n
-		n--
-	}
-	return mul
-}
-
 func main() {
-	getPermutation(4, 1)
+	// getPermutation(4, 9)
+	getPermutation(4, 5)
+	// getPermutation(4, 6)
+	// getPermutation(4, 24)
+	// getPermutation(3, 1)
+	// getPermutation(3, 3)
+	// getPermutation(3, 4)
 }
