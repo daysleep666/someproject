d2dafefb68ee9e57a73f5256981c917395a68ea9
hanzecheng <576129407@qq.com>
2019-04-03 13:30:30 +0800

    .

--- datastruct/graph/dijkstra/main.go
+++ datastruct/graph/dijkstra/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 带权有向图
 
 const (
@@ -20,34 +22,67 @@ func (g *Graph) Add(f, t, w int) {
 	g.E[t] = append(g.E[t], &Edge{To: f, Weight: w})
 }
 
-func (g *Graph) dijkstra() {
-	s, u := make(map[int]int), make(map[int]int)
-	for _, e := range g.E {
-		for k, _ := range e {
-			u[k] = max
-		}
+func (g *Graph) dijkstra(node int) {
+	u, s := make(map[int]int), make(map[int]int)
+	// 初始化当前节点的距离为0，其他节点的距离为max
+	for k, _ := range g.E {
+		u[k] = max
 	}
-	first := 1
-	s[first] = 0
-	list := []int{first}
+	u[node] = 0
+	s[node] = 0
+
+	list := []int{node}
 	for len(list) != 0 {
-		for _, v := range list {
-			nodes := g.E[v]
-			for _, node := range nodes {
-				u[node.To] = node.Weight
+		for i := 0; i < len(list); i++ {
+			cur := list[i]
+			// 1.找出当前节点到所有其他节点的距离更新到u中
+			es := g.E[cur]
+			for _, e := range es {
+				// 如果从初始节点到当前节点的距离加上当前节点到新节点的距离小于u中记录的距离
+				newWeight := (s[cur] + e.Weight)
+				if newWeight < u[e.To] {
+					u[e.To] = newWeight
+				}
 			}
+			// 2.选出距离最短的节点加入s中
+			minEdges := make([]*Edge, 0)
+			for k, v := range u {
+				_, isExist := s[k]
+				if (len(minEdges) == 0 || minEdges[0].Weight >= v) && !isExist {
+					if len(minEdges) == 0 || minEdges[0].Weight > v {
+						minEdges = []*Edge{&Edge{To: k, Weight: v}}
+					} else {
+						minEdges = append(minEdges, &Edge{To: k, Weight: v})
+					}
+				}
+			}
+			list = make([]int, 0, len(minEdges))
+			for _, v := range minEdges {
+				s[v.To] = v.Weight
+				list = append(list, v.To)
+			}
+			// 重复1
 		}
 	}
 
-	mink, minv := 0, max
-	for k, v := range u {
-		if v < minv {
-			mink, minv = k, v
-		}
+	for k, v := range s {
+		fmt.Printf("%v->%v:%v \n", node, k, v)
 	}
-	// s[mink] = minv
+
 }
 
 func main() {
-
+	g := Graph{make(map[int][]*Edge, 0)}
+	g.Add(1, 2, 1)
+	g.Add(1, 3, 3)
+	g.Add(1, 4, 4)
+	g.Add(2, 3, 1)
+	g.Add(3, 4, 1)
+	g.Add(2, 5, 1)
+	// for k, v := range g.E {
+	// 	for _, vv := range v {
+	// 		fmt.Printf("%v->%v:%v\n", k, vv.To, vv.Weight)
+	// 	}
+	// }
+	g.dijkstra(1)
 }
---
+++ leetcode/386. 字典序排数/main.go
@@ -0,0 +1,32 @@
+package main
+
+// func lexicalOrder(n int) []int {
+// 	i := 1
+// 	for i < n {
+// 		i *= 10
+// 	}
+// 	i /= 10
+// 	fmt.Println("i = ", i)
+// 	for from := i; from <= n; from++ {
+// 		tmp := from
+// 		for tmp%10 == 0 {
+// 			tmp /= 10
+// 			fmt.Printf("%v ", tmp)
+// 		}
+// 		fmt.Printf("%v ", tmp)
+// 	}
+// 	return nil
+// }
+
+// func d(start, end int) {
+// 	if end-start < 10 {
+
+// 	}
+// 	for i := start; i < end; i++ {
+
+// 	}
+// }
+
+func main() {
+	// lexicalOrder(111)
+}
