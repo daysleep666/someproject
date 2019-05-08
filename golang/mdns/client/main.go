70c29973f425d4d8a38115547142e9bd47e1078d
hanzecheng <daysleep666@gmail.com>
2019-05-08 16:38:57 +0800

    .

---
+++ golang/mdns/client/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/micro/mdns"
+)
+
+func main() {
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	go func() {
+		for entry := range entriesCh {
+			fmt.Printf("Got new entry: %v\n", entry)
+		}
+	}()
+
+	// Start the lookup
+	mdns.Lookup("_foobar._tcp", entriesCh)
+	close(entriesCh)
+	select {}
+}
