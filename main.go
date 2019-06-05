abd0872ada6867e31ae7a81eb7841b8cf776d1aa
hanzecheng <daysleep666@gmail.com>
2019-06-05 20:00:59 +0800

    /

--- main.go
+++ main.go
@@ -1,108 +1,175 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	"bufio"
+	"io"
+	"strings"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
+
+	"fmt"
+
+	"path/filepath"
+
+	"os"
 )
 
-var INITTIMESTAMP uint64
-var index, WORKID uint64
-var lastTimeStamp uint64
-var mt sync.Mutex
+var Mdomain map[string]string
 
 func init() {
-	INITTIMESTAMP = uint64(time.Date(2018, 1, 1, 0, 0, 0, 0, time.Now().Location()).UnixNano())
-	WORKID = 1
+	Mdomain = make(map[string]string)
 }
 
-func main() {
-	var wg sync.WaitGroup
-	var count int64 = 100
-	for i := int64(0); i < count; i++ {
-		wg.Add(int(1))
-		go func() {
-			for j := int64(0); j < 1; j++ {
-				snowFlake()
-				// fmt.Println(snowFlake(), lastTimeStamp, index)
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	"encoding/json"
-	"fmt"
-	"time"
-
-	"github.com/liunian1004/pdd"
+const (
+	pathName = "./tmp"
+	fileName = "domain.txt"
 )
 
-func main() {
-	{
-		a := "aaa"
-		defer fmt.Println(a, "222")
-	}
-	fmt.Println("111")
+type Worker struct {
+	watch      *fsnotify.Watcher
+	reloadChan chan int
+}
 
-	return
-	p := pdd.NewPdd(&pdd.Config{
-		ClientId:     "",
-		ClientSecret: "",
-		RetryTimes:   1, // 设置接口调用失败重试次数
+//监控目录
+
+func (w *Worker) watchDir(dir string) {
+
+	//通过Walk来遍历目录下的所有子目录
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+
+		//这里判断是否为目录，只需监控目录即可
+
+		//目录下的文件也在监控范围内，不需要我们一个一个加
+
+		if info.IsDir() {
+
+			path, err := filepath.Abs(path)
+
+			if err != nil {
+
+				return err
+
+			}
+
+			err = w.watch.Add(path)
+
+			if err != nil {
+
+				return err
+
+			}
+
+			fmt.Println("监控 : ", path)
+
+		}
+
+		return nil
+
 	})
 
-	// 初始化多多客相关 API 调用
-	d := p.GetDDK()
+	go func() {
 
-	// // 获取主题列表
-	// r, err := d.ThemeListGet(1, 20)
+		for {
 
-	// 初始化商品 API
-	// g := p.GetGoodsAPI()
-	// for i := 1; i < 100; i++ {
-	// 	gs, _ := d.GoodsSearch(pdd.Params{"page": i})
-	// 	for _, v := range gs.GoodsList {
-	// 		// r, _ := json.Marshal(v)
-	// 		// fmt.Println(string(r))
-	// 		if v.MinNormalPrice < 300 {
-	// 			fmt.Println(v.GoodsName, v.GoodsDesc, v.MinNormalPrice, v.GoodsId)
-	// 		}
-	// 	}
-	// }
-	// 3864561472
-	res, err := d.GoodsPromotionUrlGenerate("8472748_56301691", 8395580848, pdd.Params{"custom_parameters": "测试测试", "generate_weapp_webview": true, "generate_we_app": true})
-	st, _ := json.Marshal(res)
-	fmt.Println(res.Url, err)
-	fmt.Println(string(st))
-	res1, _ := d.OrderListIncrementGet(time.Now().Unix()-86400, time.Now().Unix())
-	// fmt.Println("count:", res1.TotalCount)
-	// for _, v := range res1.OrderList {
-	// 	fmt.Println(v.)
-	// }
-	// res2, _ := d.ThemeListGet(1, 10)
-	// for _, v := range res2.ThemeList {
-	// 	fmt.Println(v)
-	// }
+			select {
+
+			case ev := <-w.watch.Events:
+				if ev.Op&fsnotify.Write == fsnotify.Write {
+
+					fmt.Println("写入文件 : ", ev.Name)
+					w.reloadChan <- 1
+				}
+
+			case err := <-w.watch.Errors:
+
+				{
+
+					fmt.Println("error : ", err)
+
+					return
+
+				}
+
+			}
+
+		}
+
+	}()
 
 }
 
-func snowFlake() uint64 {
-	mt.Lock()
-	defer mt.Unlock()
+func ListenAndReload() {
+	watch, _ := fsnotify.NewWatcher()
 
-	timeNow := uint64(time.Now().UnixNano())
-	if lastTimeStamp == timeNow {
-		index = (index + 1) & 111111111111
-		if index == 0 {
-			for lastTimeStamp == timeNow {
-				timeNow = uint64(time.Now().UnixNano())
-			}
+	w := Worker{
+		watch:      watch,
+		reloadChan: make(chan int),
+	}
+
+	w.watchDir(pathName)
+
+	for {
+		res, err := w.reloadFile(pathName + "/" + fileName)
+		if err != nil {
+			fmt.Printf("文件读取错误,请尽快修改.[%v]\n", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		m, err := w.reloadMap(res)
+		if err != nil {
+			fmt.Printf("文件读取错误,请尽快修改.[%v]\n", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		if len(m) == 0 {
+			fmt.Printf("当前无数据\n")
+			time.Sleep(time.Second)
+			continue
+		}
+		Mdomain = m
+		fmt.Println(Mdomain)
+		<-w.reloadChan
+	}
+}
+
+func (w *Worker) reloadFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	var result []string
+	buf := bufio.NewReader(file)
+	for {
+		line, err := buf.ReadString('\n') //以'\n'为结束符读入一行
+		result = append(result, line)
+		if err != nil || io.EOF == err {
+			break
 		}
 
-	} else {
-		index = 0
 	}
-	lastTimeStamp = timeNow
-	fmt.Printf("%v\n", ((timeNow-INITTIMESTAMP)<<22 | WORKID<<12 | index))
-	return ((timeNow-INITTIMESTAMP)<<22 | WORKID<<12 | index)
+	return result, nil
+}
+
+func (w *Worker) reloadMap(res []string) (map[string]string, error) {
+	m := make(map[string]string)
+	for _, str := range res {
+		strs := strings.Split(str, "=")
+		if len(strs) != 2 {
+			return nil, fmt.Errorf("%v_length_is_not_2", strs)
+		}
+		strs[0] = strings.TrimSpace(strs[0])
+		strs[1] = strings.TrimSpace(strs[1])
+		m[strs[0]] = strs[1]
+	}
+	return m, nil
+}
+
+func main() {
+
+	ListenAndReload()
+
+	select {}
+
 }
